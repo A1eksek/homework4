b638d4f2a549940d211400eb0df67067366ba1e8
A1lekse <alexey20080705@gmail.com>
2024-06-11 17:36:16 +0400

    Исправил ошибки

--- main.py
+++ main.py
@@ -1,38 +1,40 @@
-# a = input("Введите арифметическое выражение")
-# if '+' in list(a):
-#     numbers = a.split('+')
-#     num1 = int(numbers[0])
-#     num2 = int(numbers[1])
-#     print("Сумма чисел: ", num1+num2)
-# elif '*' in list(a):
-#     numbers = a.split('*')
-#     num1 = int(numbers[0])
-#     num2 = int(numbers[1])
-#     print("Произведение чисел: ", num1*num2)
-# elif '-' in list(a):
-#     numbers = a.split('-')
-#     num1 = int(numbers[0])
-#     num2 = int(numbers[1])
-#     print("Разность чисел: ", num1-num2)
-# elif '/' in list(a):
-#     numbers = a.split('/')
-#     num1 = int(numbers[0])
-#     num2 = int(numbers[1])
-#     print("Частное чисел: ", num1/num2)
+a = input("Введите арифметическое выражение")
+if '+' in list(a):
+    numbers = a.split('+')
+    num1 = int(numbers[0])
+    num2 = int(numbers[1])
+    print("Сумма чисел: ", num1+num2)
+elif '*' in list(a):
+    numbers = a.split('*')
+    num1 = int(numbers[0])
+    num2 = int(numbers[1])
+    print("Произведение чисел: ", num1*num2)
+elif '-' in list(a):
+    numbers = a.split('-')
+    num1 = int(numbers[0])
+    num2 = int(numbers[1])
+    print("Разность чисел: ", num1-num2)
+elif '/' in list(a):
+    numbers = a.split('/')
+    num1 = int(numbers[0])
+    num2 = int(numbers[1])
+    print("Частное чисел: ", num1/num2)
 
 
 import random
-# list1 = [random.randint(-20, 50) for x in range(20)]
+list1 = [random.randint(-20, 50) for x in range(20)]
 # list1 = [1, 0, -20, -3, 4, -8, 32, 43, -4, 5, -2]
-# for i in range(len(list1)):
-#     if list1[i] > 0:
-#         k=+1
-#     elif list1[i] < 0:
-#         l=+1
-#
-# print(list1)
-# print("Минимальное значение из списка: ", min(list1))
-# print("Максимальное значение из списка: ", max(list1))
-# print("Кол-во отрицательных чисел: ", l)
-# print("Кол-во положительных чисел чисел: ", k)
-# print("Нули: ",list1.count(0))
+l = 0
+k = 0
+for i in range(len(list1)):
+    if list1[i] > 0:
+        k = k+1
+    elif list1[i] < 0:
+        l = l+1
+
+print(list1)
+print("Минимальное значение из списка: ", min(list1))
+print("Максимальное значение из списка: ", max(list1))
+print("Кол-во отрицательных чисел: ", l)
+print("Кол-во положительных чисел чисел: ", k)
+print("Нули: ",list1.count(0))
